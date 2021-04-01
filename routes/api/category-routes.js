e6bc2cc7305791743457da2837910767e51c4ebb
noheropage <mursepage@gmail.com>
2021-04-01 11:56:24 -0700

    Tidy up code for routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body)
@@ -46,7 +46,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(req.body, {
@@ -55,7 +55,7 @@ router.put('/:id', (req, res) => {
       },
     })
     if (!categoryData) {
-      res.status(404).json({ message: 'No tag found with this id' })
+      res.status(404).json({ message: 'No category found with this id' })
       return;
     }
 
@@ -65,7 +65,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
@@ -75,7 +75,7 @@ router.delete('/:id', (req, res) => {
     })
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No tag found with this id' })
+      res.status(404).json({ message: 'No category found with this id' })
       return;
     }
 
